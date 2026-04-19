12d5830668d33fed4669516a414dcef834c69da8
teamexpertbs <thekingofcbr21@gmail.com>
2026-04-19 14:36:04 +0530

    fix: correct cacheeNames typo and add icon caching in sw.js

--- artifacts/craka-osint/public/sw.js
+++ artifacts/craka-osint/public/sw.js
@@ -1,9 +1,19 @@
-const CACHE_NAME = 'craka-osint-v1';
+const CACHE_NAME = 'craka-osint-v2';
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/favicon.svg'
+  '/favicon.svg',
+  '/android/launchericon-48x48.png',
+  '/android/launchericon-72x72.png',
+  '/android/launchericon-96x96.png',
+  '/android/launchericon-144x144.png',
+  '/android/launchericon-192x192.png',
+  '/android/launchericon-512x512.png',
+  '/ios/180.png',
+  '/ios/152.png',
+  '/ios/167.png',
+  '/windows/Square150x150Logo.scale-200.png'
 ];
 
 self.addEventListener('install', function(event) {
@@ -20,7 +30,7 @@ self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
-        cacheeNames.filter(function(cacheName) {
+        cacheNames.filter(function(cacheName) {
           return cacheName !== CACHE_NAME;
         }).map(function(cacheName) {
           return caches.delete(cacheName);
@@ -32,7 +42,7 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
- event.respondWith(
+  event.respondWith(
     caches.match(event.request).then(function(response) {
       if (response) {
         return response;
